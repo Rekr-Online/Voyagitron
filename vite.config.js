3605bc95dd48ba2ea5f4b90e9aacae61d061f197
Leonardo RAIMUNDO <leonardoraimundo.f@gmail.com>
2026-04-24 17:19:27 +0200

    Ajout PWA vite-plugin-pwa

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Voyagitron',
+        short_name: 'Voyagitron',
+        description: 'Ton application de voyage gratuite',
+        theme_color: '#1a1a2e',
+        background_color: '#1a1a2e',
+        display: 'standalone',
+        icons: [
+          {
+            src: '/icons.svg',
+            sizes: 'any',
+            type: 'image/svg+xml'
+          }
+        ]
+      }
+    })
+  ],
 })
